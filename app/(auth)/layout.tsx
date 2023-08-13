7cc46f502ec96e0b31d3e1171b3b0d5a4a16a3f5
v-Sharan <vsharansago@gmail.com>
2023-08-13 22:47:20 +0530

    added dark theme in clerk

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import '../globals.css'
+import { dark } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,12 +19,12 @@ export default function RootLayout({
   }) {
     return (
         <ClerkProvider
-        // appearance={{
-        //   baseTheme: dark,
-        // }}
+        appearance={{
+          baseTheme: dark,
+        }}
       >
         <html lang='en'>
-          <body className={`${inter.className} bg-dark-1`}>{children}</body>
+          <body className={`${inter.className} flex justify-center items-center h-[100vh] bg-dark-1`}>{children}</body>
         </html>
       </ClerkProvider>
     );
